6b4ec2ebb873cf2fe1fe4c16b91e5c34a3197d72
Paula <onotseike@hotmail.com>
2021-10-31 20:10:45 +0100

    Commenced work on UI

---
+++ Demo/Demo/Demo.Shared/ViewModels/MainVM.cs
@@ -0,0 +1,33 @@
+﻿using Demo.Helpers;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.ViewModels
+{
+    public class MainVM : BaseNotifyClass
+    {
+        #region Properties
+
+        public ClientsVM ClientsVM { get; private set; }
+        public HomeVM HomeVM { get; private set; }
+        public InvoicesVM InvoicesVM { get; private set; }
+        public SettingsVM SettingsVM { get; private set; }
+
+        #endregion
+
+        #region Constructor(s)
+
+        public MainVM()
+        {
+            ClientsVM = new ClientsVM();
+            HomeVM = new HomeVM();
+            InvoicesVM = new InvoicesVM();
+            SettingsVM = new SettingsVM();
+        }
+
+
+        #endregion
+    }
+}
---
+++ Demo/Demo/Demo.Shared/ViewModels/SettingsVM.cs
@@ -0,0 +1,12 @@
+﻿using Demo.Helpers;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.ViewModels
+{
+    public class SettingsVM : BaseNotifyClass
+    {
+    }
+}
