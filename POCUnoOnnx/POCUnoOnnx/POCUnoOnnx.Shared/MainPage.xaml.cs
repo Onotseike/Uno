df29e828cf246b2625f68af14c4343b6315bbe5d
Paula <onotseike@hotmail.com>
2022-08-28 21:22:46 +0100

    Working with embedded resource

--- POCUnoOnnx/POCUnoOnnx/POCUnoOnnx.Shared/MainPage.xaml.cs
+++ POCUnoOnnx/POCUnoOnnx/POCUnoOnnx.Shared/MainPage.xaml.cs
@@ -1,19 +1,17 @@
-﻿using Microsoft.UI.Xaml;
+﻿
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using System.Reflection;
+using System.Threading.Tasks;
 
 using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.Media.Capture;
+using Windows.Storage;
+
+using Exception = System.Exception;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -24,9 +22,103 @@ namespace POCUnoOnnx
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private string currentFile;
+        
+        public string[] EmbeddedResources { get; set; } = typeof(MainPage).GetTypeInfo().Assembly.GetManifestResourceNames();
+        
+        public string CurrentFile
+        {
+            get => currentFile;
+            set
+            {
+                currentFile = value;
+                using (var resource = typeof(MainPage).Assembly.GetManifestResourceStream(value))
+                {
+                    try
+                    {
+                        var reader = new StreamReader(resource);
+                        content.Text = reader.ReadToEnd();
+                    }
+                    catch (Exception exception)
+                    {
+                        content.Text = $"Error: {exception.Message}";
+                    }
+                }
+            }
+        }
+
+        MobileNetImageClassifier _classifier;
+        
         public MainPage()
         {
             this.InitializeComponent();
+            _classifier = new MobileNetImageClassifier(EmbeddedResources);
+        }
+
+        async Task RunInferenceAsync()
+        {
+            runClassifer.IsEnabled = false;
+
+            try
+            {
+                var sampleImage = await _classifier.GetSampleImageAsync();
+                var result = await _classifier.GetClassificationAsync(sampleImage);
+
+                content.Text = $"The Result is: {result}";
+            }
+            catch (Exception exception)
+            {
+                content.Text = $"Error: {exception.Message}";
+            }
+            finally
+            {
+                runClassifer.IsEnabled = true;
+            }
+        }
+
+        private void runClassifer_Click(object sender, RoutedEventArgs e)
+        {
+            _ = RunInferenceAsync();
+        }
+
+        public async Task<byte[]> GetBytesFromFile(StorageFile file)
+        {
+            var stream = await file.OpenStreamForReadAsync();
+            byte[] bytes = new byte[stream.Length];
+            await stream.ReadAsync(bytes, 0, bytes.Length);
+            stream.Seek(0, SeekOrigin.Begin);
+            return bytes;
+        }
+
+        private async void selectImage_Click(object sender, RoutedEventArgs e)
+        {
+            runClassifer.IsEnabled = false;
+            try
+            {
+                var captureUI = new CameraCaptureUI();
+                captureUI.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
+                captureUI.PhotoSettings.CroppedSizeInPixels = new Size(224, 224);
+
+                var photo = await captureUI.CaptureFileAsync(CameraCaptureUIMode.Photo);
+                if (photo == null)
+                {
+                    return;
+                }
+                else
+                {
+                    var sourceImage = await GetBytesFromFile(photo);
+                    var result = await _classifier.GetClassificationAsync(sourceImage);
+                }
+            }
+            catch (Exception exception)
+            {
+
+                content.Text = $"Error: {exception.Message}";
+            }
+            finally
+            {
+                runClassifer.IsEnabled = true;
+            }
         }
     }
 }
