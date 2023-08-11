585ba409fbcdac3b72cd752df19ba7c1496b4e44
Paula <onotseike@hotmail.com>
2023-08-11 14:15:42 -0400

    initial commit

---
+++ UnoScrollReveal/UnoScrollReveal/UnoScrollReveal/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+namespace UnoScrollReveal
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
